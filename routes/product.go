c45dda5701abe6a829d3c4a127567af42f9865ca
muhduzairmf <uzair.developer@outlook.com>
2022-03-01 01:20:47 +0800

    Update product route

---
+++ routes/product.go
@@ -0,0 +1,225 @@
+package routes
+
+import (
+	"restapi-with-fiber-and-gorm/database"
+	"restapi-with-fiber-and-gorm/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// GET http://localhost:3005/api/product
+func GetAllProduct(c *fiber.Ctx) error {
+	allProduct := []models.Product{}
+
+	result := database.Database.Db.Find(&allProduct)
+	if result.Error != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"status": "Server error",
+			"message": "Error occured when getting all data about product",
+			"error": result.Error,
+		})
+	}
+
+	return c.Status(200).JSON(fiber.Map{
+		"status": "Ok",
+		"message": "Successfully getting all users data",
+		"data": allProduct,
+	})
+}
+
+// GET http://localhost:3005/api/product/:id
+func GetProductById(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The id params must be an integer",
+			"error": err.Error(),
+		})
+	}
+
+	if id < 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The id params must be a positive integer",
+			"error": "The id params has value less than zero (0).",
+		})
+	}
+
+	var theProduct models.Product
+	
+	database.Database.Db.Find(&theProduct, "id = ?", id)
+	if theProduct.ID == 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The product does not exist",
+			"error": "Cannot find product with the given id",
+		})
+	}
+
+	return c.Status(200).JSON(fiber.Map{
+		"status": "Ok",
+		"message": "Successfully getting product by id",
+		"data": theProduct,
+	})
+}
+
+// POST http://localhost:3005/api/product
+func CreateProduct(c *fiber.Ctx) error {
+	var newProduct models.Product
+
+	err := c.BodyParser(&newProduct)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "Error occured when parsing the request body",
+			"error": err.Error(),
+		})
+	}
+
+	if newProduct.Name == "" || newProduct.SerialNumber == "" || newProduct.TotalStock == 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "Please include name (string), serialNumber (string) and totalStock (integer)",
+			"error": "Important field is empthy",
+		})
+	}
+
+	if newProduct.TotalStock <= 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The totalStock field must be at least 1 for creating a new product",
+			"error": "totalStock value is less than or equal to 0",
+		})
+	}
+
+	result := database.Database.Db.Create(&newProduct)
+	if result.Error != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "Error occured when creating data",
+			"error": result.Error,
+		})
+	}
+
+	return c.Status(200).JSON(fiber.Map{
+		"status": "Ok",
+		"message": "Successfully create a new product",
+		"data": newProduct,
+	})
+}
+
+// PATCH http://localhost:3005/api/product/:id
+func UpdateProduct(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The id params must be an integer",
+			"error": err.Error(),
+		})
+	}
+
+	if id < 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The id params must be a positive integer",
+			"error": "The id params has value less than zero (0).",
+		})
+	}
+
+	var theProduct models.Product
+
+	database.Database.Db.Find(&theProduct, "id = ?", id)
+	if theProduct.ID == 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The product does not exist",
+			"error": "Cannot find product with the given id",
+		})
+	}
+
+	var updatedProduct models.Product
+
+	err = c.BodyParser(&updatedProduct)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "Error occured when parsing the request body",
+			"error": err.Error(),
+		})
+	}
+
+	if updatedProduct.Name == "" || updatedProduct.SerialNumber == "" {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "Please include name (string) and serialNumber (string)",
+			"error": "Important field is empthy",
+		})
+	}
+
+	if updatedProduct.TotalStock < 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The totalStock field must be at least 0 for updating",
+			"error": "totalStock value is less than 0",
+		})
+	}
+
+	theProduct.Name = updatedProduct.Name
+	theProduct.SerialNumber = updatedProduct.SerialNumber
+	theProduct.TotalStock = updatedProduct.TotalStock
+
+	database.Database.Db.Save(&theProduct)
+
+	return c.Status(200).JSON(fiber.Map{
+		"status": "Ok",
+		"message": "Successfully update the product",
+		"data": theProduct,
+	})
+}
+
+// DELETE http://localhost:3005/api/product/:id
+func DeleteProduct(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The id params must be an integer",
+			"error": err.Error(),
+		})
+	}
+
+	if id < 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The id params must be a positive integer",
+			"error": "The id params has value less than zero (0).",
+		})
+	}
+
+	var theProduct models.Product
+
+	database.Database.Db.Find(&theProduct, "id = ?", id)
+	if theProduct.ID == 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "The product does not exist",
+			"error": "Cannot find product with the given id",
+		})
+	}
+
+	err = database.Database.Db.Delete(&theProduct).Error
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "Bad request",
+			"message": "Delete operation failed",
+			"error": err.Error(),
+		})
+	}
+
+	return c.Status(200).JSON(fiber.Map{
+		"status": "Ok",
+		"message": "Successfully delete the user",
+	}) 
+}
