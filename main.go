a193e65bd79f9e55120befa5e3e1f2dea86b59a7
muhduzairmf <uzair.developer@outlook.com>
2022-03-01 01:43:25 +0800

    Update main.go

--- main.go
+++ main.go
@@ -16,6 +16,20 @@ func userRoute(app *fiber.App) {
 	app.Delete("/api/user/:id", routes.DeleteUser)
 }
 
+func productRoute(app *fiber.App) {
+	app.Get("/api/product", routes.GetAllProduct)
+	app.Get("/api/product/:id", routes.GetProductById)
+	app.Post("/api/product", routes.CreateProduct)
+	app.Patch("/api/product/:id", routes.UpdateProduct)
+	app.Delete("/api/product/:id", routes.DeleteProduct)
+}
+
+func orderRoute(app *fiber.App) {
+	app.Get("/api/order", routes.GetAllOrder)
+	app.Get("/api/order/:id", routes.GetOrderById)
+	app.Post("/api/order", routes.CreateOrder)
+}
+
 func notFoundRoute(app *fiber.App) {
 	app.Get("/*", func(c *fiber.Ctx) error {
 		return c.Status(404).JSON(fiber.Map{
@@ -38,6 +52,10 @@ func main() {
 	})
 
 	userRoute(app)
+
+	productRoute(app)
+
+	orderRoute(app)
 
 	notFoundRoute(app)
 
